bd4b41660bb9d50a762f72b6fe5467ac3d2ed25c
Elen <elenaydith@gmail.com>
2024-05-31 12:06:31 +0700

    responsive

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,12 +1,8 @@
 <!DOCTYPE html>
-
-<html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default" data-assets-path="../assets/"
-    data-template="vertical-menu-template-free">
-
+<html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default" data-assets-path="../assets/" data-template="vertical-menu-template-free">
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
     <title>Login</title>
 
@@ -18,27 +14,21 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 
     <link rel="stylesheet" href="{{asset('admin_theme/assets/vendor/fonts/boxicons.css')}}" />
-    <link rel="stylesheet" href="{{asset('admin_theme/assets/vendor/css/core.css')}}"
-        class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{asset('admin_theme/assets/vendor/css/theme-default.css')}}"
-        class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{asset('admin_theme/assets/vendor/css/core.css')}}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{asset('admin_theme/assets/vendor/css/theme-default.css')}}" class="template-customizer-theme-css" />
     <link rel="stylesheet" href="{{asset('admin_theme/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}" />
     <script src="{{asset('admin_theme/assets/vendor/js/helpers.js')}}"></script>
     <script src="{{asset('admin_theme/assets/js/config.js')}}"></script>
-
     <link rel="stylesheet" href="{{asset('toastr/toastr.min.css') }}">
 
     <style>
-        input[type="text"] {
+        input[type="text"], input[type="email"], input[type="password"] {
             border-top: 0;
             border-right: 0;
             border-left: 0;
-
             -webkit-box-shadow: none;
             box-shadow: none;
         }
@@ -47,18 +37,41 @@
             background-color: #31534C;
             border-color: #31534C;
         }
+
+        .card {
+            max-width: 900px;
+            margin: auto;
+        }
+
+        .w-50 img {
+            max-width: 100%;
+            height: auto;
+        }
+
+        @media (max-width: 768px) {
+            .d-flex.flex-row {
+                flex-direction: column;
+            }
+            .w-50 {
+                width: 100% !important;
+            }
+            .py-5 {
+                padding: 1rem !important;
+            }
+            .px-3 {
+                padding: 1rem !important;
+            }
+            .px-5 {
+                padding: 1rem !important;
+            }
+        }
     </style>
-
 </head>
-
 <body>
-
-
     <div class="container">
-
         <div class="card my-5">
             <div class="d-flex flex-row">
-                <div class="w-50 d-flex align-items-center justify-content-center" style="background-color : #31534C;">
+                <div class="w-50 d-flex align-items-center justify-content-center" style="background-color: #31534C;">
                     <img src="{{asset('login_assets/img/logo.png')}}" width="200" alt="">
                 </div>
                 <div class="w-50 py-5 px-3">
@@ -72,8 +85,7 @@
                         <form action="{{url('postlogin')}}" method="post">
                             @csrf
                             @if(session()->has('message'))
-                            <div class="toastrDefaultSuccess" role="alert" id="notif">
-                            </div>
+                            <div class="toastrDefaultSuccess" role="alert" id="notif"></div>
                             @endif
 
                             @if(session()->has('error'))
@@ -104,9 +116,7 @@
                             <div class="d-flex justify-content-between mt-4 mb-1">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" id="remember-me">
-                                    <label class="form-check-label" for="remember-me">
-                                        Remember Me
-                                    </label>
+                                    <label class="form-check-label" for="remember-me">Remember Me</label>
                                 </div>
                                 <button class="btn btn-success px-5" type="submit">Login</button>
                             </div>
@@ -118,15 +128,11 @@
                             <div class="text-center mt-2 mb-2">
                                 <a href="{{url('/')}}">Klinik Fanda Berkat Medika Panti</a>
                             </div>
-
-                            
                         </form>
-
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
 
     <!-- Core JS -->
@@ -137,10 +143,5 @@
     <script src="{{asset('admin_theme/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
     <script src="{{asset('admin_theme/assets/vendor/js/menu.js')}}"></script>
     <script src="{{asset('admin_theme/assets/js/main.js')}}"></script>
-
-
-
-
 </body>
-
 </html>
